becbdc07cb5a769d99607e5b63b28dbbf36f6582
Luca Hentschel <luca.hentschel@2peaches.de>
2026-01-26 08:32:22 +0100

    fix: correct parameter order in registerCommands for Discord integration

--- src/registerCommands.ts
+++ src/registerCommands.ts
@@ -1,4 +1,4 @@
 import { registerCommands } from "honocord";
-import * as handlers from "./routes/discord/commands/index";
+import * as commands from "./routes/discord/commands/index";
 
-await registerCommands(process.env.DISCORD_APPLICATION_ID!, process.env.DISCORD_TOKEN!, Object.values(handlers));
+await registerCommands(process.env.DISCORD_TOKEN!, process.env.DISCORD_APPLICATION_ID!, Object.values(commands));
